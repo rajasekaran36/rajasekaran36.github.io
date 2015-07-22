e1b6dc990ed8b94edfcd5a75e16eb416f9f92d2a
rajasekaran <rajasekaran@tux-machine>
2015-07-22 17:03:20 +0530

    button not working

---
+++ scripts/myscripts.js
@@ -0,0 +1,6 @@
+function getdata(){
+	var name=document.getElementById("name").value;
+	var rollno=document.getElementById("rolno").value;
+	//document.getElementById("display").innerHTML=name;
+	alert("hello");
+}
