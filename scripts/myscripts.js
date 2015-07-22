19ee9c6a9521c8127e62150938a819b697f0af58
rajasekaran <rajasekaran@tux-machine>
2015-07-22 17:20:49 +0530

    run

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,6 +1,13 @@
 function getdata(){
 	var name=document.getElementById("name").value;
-	var rollno=document.getElementById("rolno").value;
-	//document.getElementById("display").innerHTML=name;
-	alert("hello");
+	var rollno=document.getElementById("rollno").value;
+	var year=document.getElementById("year").value;
+}
+function pre(){
+	var d=new Date();
+	var pd=document.getElementById("date");
+	var put=d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear();
+	pd.value=put;
+	//alert(put);
+	//pd.innerHTML=put;
 }
