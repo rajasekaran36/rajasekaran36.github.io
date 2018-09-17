d73a990c5d86c011c87992bf89c1e6f8c0a8b2a6
Rajasekaran S <proffraja@gmail.com>
2018-09-17 16:14:28 +0530

    Last Update 17-09-2018 16:14:28.05

--- adds/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"arithmatic","c":"Addition","l":"Addition(int, int)"},{"p":"arithmatic","c":"Addition","l":"addOp(int, int)"},{"p":"arithmatic","c":"Addition","l":"n1"},{"p":"arithmatic","c":"Addition","l":"n2"},{"p":"arithmatic","c":"Addition","l":"result"}]
--- adds/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"arithmatic","l":"Addition"}]
